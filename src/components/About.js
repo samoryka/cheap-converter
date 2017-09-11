20e7a041b90afa92d8f0d131ac15193d78b06462
Samory Ka <ka.samory@gmail.com>
2017-09-11 14:39:58 +0200

    Removed useless import in About

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import {BrowserRouter as Router, Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 const Card = styled.div`
 border-radius: 5px;
